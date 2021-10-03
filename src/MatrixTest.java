c5f05f81284c829729514f7677fc8a2c17cba3d8
Anthony Bird <birdas273@gmail.com>
2021-10-03 13:28:50 -0400

    tested prints

---
+++ src/MatrixTest.java
@@ -0,0 +1,144 @@
+import javatrix.*;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
+public class MatrixTest {
+
+	@Test
+	public void testTimes1() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Matrix a = new Matrix(2, 3, 2.0);
+		Matrix b = new Matrix(3, 4, 1.0);
+		Matrix M = a.times(b);
+		double[][] m = M.getMatrix();
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		if (M.getLength() != 2) 
+			fail("Length incorrect should be 2 for matrices of 2x3 times 3x4.\n");
+
+		if (M.getWidth() != 4)
+			fail("Width incorrect should be 4 for matrices of 2x3 times 3x4.\n");
+
+		// Check for desired result
+		for (int i = 0; i < M.getLength(); ++i) {
+			for (int j = 0; j < M.getWidth(); ++j) {
+				if (m[i][j] != 6)
+					fail("Unexpected output.\n");
+			}
+		}		
+	}
+
+	@Test
+	public void testTimes2() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Matrix a = new Matrix(2, 3, 2.0);
+		Matrix b = new Matrix(3, 10, 1.0);
+		Matrix M = a.times(b);
+		double[][] m = M.getMatrix();
+
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		if (M.getLength() != 2) 
+			fail("Length incorrect should be 2 for matrices of 2x3 times 3x10.\n");
+
+		if (M.getWidth() != 10)
+			fail("Width incorrect should be 10 for matrices of 2x3 times 3x10.\n");
+
+		// Check for desired result
+		for (int i = 0; i < M.getLength(); ++i) {
+			for (int j = 0; j < M.getWidth(); ++j) {
+				if (m[i][j] != 6)
+					fail("Unexpected output.\n");
+			}
+		}		
+	}
+
+	@Test
+	public void testTimes3() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Matrix a = new Matrix(3, 10, 1.0);
+		double b = 6.0;
+		Matrix M = a.times(b);
+		double[][] m = M.getMatrix();
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		if (M.getLength() != 3) 
+			fail("Length incorrect should be 3 for matrix of 3x10.\n");
+
+		if (M.getWidth() != 10)
+			fail("Width incorrect should be 10 for matrix of 3x10.\n");
+
+		// Check for desired result
+		for (int i = 0; i < M.getLength(); ++i) {
+			for (int j = 0; j < M.getWidth(); ++j) {
+				if (m[i][j] != 6.0)
+					fail("Unexpected output.\n");
+			}
+		}		
+	}	
+
+	@Test
+	public void testPrint1() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Matrix a = new Matrix(3, 3, 1.0);
+		a.print(1, 2);
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		String s = baos.toString();
+		String c = "1.00 1.00 1.00\n1.00 1.00 1.00\n1.00 1.00 1.00\n";
+
+		if (! c.equals(s))
+			fail("Unexpected output:\n"+s+"\n");		
+	}
+
+	@Test
+	public void testPrint2() {
+		// Redirect System.out to a sring
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		System.setOut(new PrintStream(baos));
+		
+		// Prep done, call function of interest
+		Matrix a = new Matrix(3, 3, 1.0);
+		a.print(8, 2);
+		
+		// Restore the stdout stream
+		System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+		
+		String s = baos.toString();
+		String c = "1.00     1.00     1.00\n1.00     1.00     1.00\n1.00     1.00     1.00\n";
+
+		if (! c.equals(s))
+			fail("Unexpected output:\n"+s+"\n");
+	}
+	
+}
+	
---
+++ src/javatrix/Matrix.java
@@ -0,0 +1,137 @@
+package javatrix;
+public class Matrix {
+    private double[][] matrix;
+    private int length;
+    private int width;
+  
+    /**
+     * Constructor
+     *
+     * Construct a matrix from a 2D array.
+     * Parameters:
+     *   A - Two-dimensional array of doubles.
+    **/
+    public Matrix(double[][] A) {
+        //TODO: throw an IllegalArgumentException if all rows in A
+        //      do not have the same length
+        length = A.length;
+        width = A[0].length;
+        matrix = new double[length][width];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                matrix[i][j] = A[i][j];
+            }
+        }
+    }
+
+    /**
+     * Constructor
+     *
+     * Construct an m-by-n constant matrix.
+     * Parameters:
+     *   m - Number of rows.
+     *   n - Number of columns.
+     *   s - Fill the matrix with this scalar value.
+    **/
+    public Matrix(int m, int n, double s) {
+        length = m;
+        width = n;
+        matrix = new double[length][width];
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                matrix[i][j] = s;
+            }
+        }
+    }    
+              
+    /**
+     * times method. Multiplies the matrix by a scalar product.  
+     * @param s - the scalar.
+    **/
+    public Matrix times(double s) {
+        Matrix c = new Matrix(length, width, 0);
+        double[][] m = new double[length][width];
+        for (int i = 0; i < length; ++i)
+	        for (int j = 0; j < width; ++j)
+		        m[i][j] = s * matrix[i][j];
+        c.setMatrix(m);
+        return c;
+    }
+
+    /**
+     * times method. Multiplies the matrix by another matrix B.
+     * @param B - the matrix to multiply by. Must be of form width * p.
+     * @throws IllegalArguementException - Thrown when inner dimensions are not equal. 
+    **/
+    public Matrix times(Matrix B) {
+        Matrix c = new Matrix(length, B.getWidth(), 0);
+        if (width != B.getLength())
+	        throw new java.lang.IllegalArgumentException("Matrix inner dimensions must agree"); 
+        double[][] result = new double[length][B.getWidth()];
+        double cell = 0;
+	    double[][] b_matrix = B.getMatrix();
+	    for (int i = 0; i < result.length; ++i) {
+            for (int j = 0; j < result[0].length; ++j) {
+	            for (int k = 0; k < B.getLength(); ++k)
+		            cell += matrix[i][k] * b_matrix[k][j];
+		        result[i][j] = cell;
+		        cell = 0;
+            }
+        }
+        c.setMatrix(result);
+        return c;
+    }
+              
+    /**
+     * getMatrix method. Public accessor for the matrix field. 
+    **/
+    public double[][] getMatrix() {
+        return matrix;
+    }
+
+    /**
+     *
+    **/
+    public void setMatrix(double[][] m) {
+        matrix = m;
+    }
+
+    /**
+     * getLength method. Public accessor for the length field.
+    **/
+    public int getLength() {
+        return length;
+    }
+
+    /**
+     * getWidth method. Public accessor for the width field. 
+    **/
+    public int getWidth() {
+        return width;
+    }
+
+    /**
+     * Print
+     *
+     * Print the matrix to stdout. Line the elements up
+     * in columns with a Fortran-like 'Fw.d' style format.
+     * 
+     * Parameters:
+     *   w - Column width.
+     *   d - Number of digits after the decimal.
+    **/
+    public void print(int w, int d) {
+        String form = "%-" + String.valueOf(w) + "." + String.valueOf(d) + "f ";
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                System.out.format(form, matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Matrix a = new Matrix(3, 10, 1.0);
+        a.print(1, 2);
+    }
+}
