1b9045115b9ec8bbd7919f4627ecc624247fa098
Anthony Bird <birdas273@gmail.com>
2021-10-12 16:57:04 -0400

    worked on some more

--- src/Driver.java
+++ src/Driver.java
@@ -7,32 +7,70 @@ import javatrix.*;
  */
 public class Driver {
 
+    static Matrix m;
+    static Scanner input = new Scanner(System.in);
+
+    public static void create() {
+        System.out.println("\n1.) m * n with all entries being a real number s");
+        System.out.println("2.) m * n with inputted rows")
+        System.out.print("Please choose away to create a matrix (1 or 2): ");
+        int choice = input.nextInt();
+        if (choice == 1) {
+            System.out.print("\nEnter an m: ");
+            int m = input.nextInt();
+            System.out.print("\nEnter an n: ");
+            int n = input.nextInt();
+            System.out.print("\nEnter an s: ");
+            double s = input.nextDouble();
+            m = new Matrix(m, n, s);
+            System.out.println("\nMatrix created. Going back to initial options.");
+        }
+        else if (choice == 2) {
+            System.out.print("\nEnter an m: ");
+            int m = input.nextInt();
+            System.out.print("\nEnter an n: ");
+            int n = input.nextInt();
+            double[][] arr = new double[m][n]();
+            for (int i = 0; i < m; ++i) {
+                System.out.print("Input row " + i + ": ");
+                
+            }
+        }
+        else
+            System.out.println("\nInvalid choice. Going back to initial options.");
+    }
+
     /**
      * Main method.
      * @param args - command line args.
      */
     public static void main(String[] args) {
         int choice = -1;
-        Scanner input = new Scanner(System.in);
         while (choice != 0) {
-            println("Choose from one of these options:");
+            println("\nChoose from one of these options:");
             println("1.) Create a matrix");
             println("2.) Multiply created matrix by a scalar");
             println("3.) Multiply created matrix by another matrix");
             println("4.) Print created matrix");
             println("5.) Change the created matrix");
             println("0.) Exit");
-            print("\nInput a choice (by number): ");
+            print("Input a choice (by number): ");
             
             choice = input.nextInt();
             switch (choice) {
                 case 1: create();
+                        break;
                 case 2: scalarMultiply();
+                        break;
                 case 3: matrixMultiply();
+                        break;
                 case 4: printMatrix();
+                        break;
                 case 5: changeMatrix();
-
+                        break;
+                case 0: System.exit(0);
             }
         }
+        input.close();
     }
 }
